756af69db738c8a1441d3ccdd5efd6b16336d637
akhil-2207 <akhilmathew2207@gmail.com>
2023-04-12 22:39:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("akhil mathew")
+
+// console.log(a)
+// console.log(b)
+
+// var a = 10;
+// let b = 12; 
+
+
+var c = 1;
+var c = 2;
+c += 3;
+
+let d = 2;
+// let d = 3;
+d += 1;
+
+const e = 3; 
+// e += 1;
+
+console.log(c)
+console.log(d)
+console.log(e)
